0b487f493f5de8ff6168e218c83b6f7ce8c08d1e
Shivam <shivam.maurya14@outlook.com>
2023-01-29 01:55:25 +0530

    added home page

---
+++ src/homePage/HomePage.java
@@ -0,0 +1,10 @@
+package homePage;
+
+public class HomePage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a home Page");
+	}
+
+}
